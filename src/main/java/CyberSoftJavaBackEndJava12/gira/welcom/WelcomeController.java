9ab857e59cec54b61fd49581ed4fb1f537c70d9d
unknown <tungt149@gmail.com>
2021-07-31 15:57:26 +0700

    first commit

---
+++ src/main/java/CyberSoftJavaBackEndJava12/gira/welcom/WelcomeController.java
@@ -0,0 +1,12 @@
+package CyberSoftJavaBackEndJava12.gira.welcom;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+@GetMapping("/welcome")
+public Object Welcome() {
+	return "Welcome to Gira Apllication";
+}
+}
---
+++ src/test/java/CyberSoftJavaBackEndJava12/gira/GiraApplicationTests.java
@@ -0,0 +1,13 @@
+package CyberSoftJavaBackEndJava12.gira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
