473af6883d9f0ecef35d90d9d6e354910b7af56c
unknown <tungt149@gmail.com>
2021-08-23 13:49:18 +0700

    buoi 26

--- src/main/java/CyberSoftJavaBackEndJava12/gira/role/dto/CreateDto.java
+++ src/main/java/CyberSoftJavaBackEndJava12/gira/role/dto/CreateDto.java
@@ -3,9 +3,12 @@ package CyberSoftJavaBackEndJava12.gira.role.dto;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import CyberSoftJavaBackEndJava12.gira.role.validation.anotation.UniqueRoleName;
+
 public class CreateDto {
 	@NotBlank(message = "{role.name.notblank}")
 	@Size(min=3,max=50,message = "{role.name.size}")
+	@UniqueRoleName()
 	private String name;
 	@NotBlank
 	private String desciption;
